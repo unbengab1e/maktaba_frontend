e0284a72ffbb4a0727d7760194d91c6e92f6aa50
张昀祥 <21351055>
2024-06-10 19:01:33 +0800

    fix sth

--- src/views/BookshelfIndexView.vue
+++ src/views/BookshelfIndexView.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="w-full h-full flex flex-wrap ml-5 ">
-    <NormalBookCard v-for="book in books"  :img-src="book.img" :book-name="book.name" :book-reference="book.bookReference" :book-author="book.author" :book-score="book.score" :book-tag="book.tag" :is-read="book.isRead" class="m-4">
+  <div class="container flex flex-wrap justify-around ml-5 ">
+    <NormalBookCard v-for="book in books"  :img-src="book.img" :book-name="book.name" :book-author="book.author" :book-score="book.score" :book-tag="book.tag" :is-read="book.isRead" :book-reference="book['mess']" >
 
     </NormalBookCard>
   </div>
@@ -23,7 +23,6 @@ onMounted(async () => {
     console.log(dres);
     books.value.push(dres.data)
   }
-  //console.log(books.value.length)
 })
 
 
